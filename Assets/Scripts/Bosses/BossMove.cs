b852a4f636562ac6e5bac256ca2f8ff7b24e7024
Bintang Khalik <bintangkhaliqq@gmail.com>
2026-06-22 00:25:42 +0700

    added Boss Controller

---
+++ Assets/Scripts/Bosses/BossMove.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+public class BossMove : MonoBehaviour 
+{
+    protected Action ActionEvent;
+    protected float Duration;
+    protected float Cooldown;
+    protected bool IsReady;
+
+    public IEnumerator Execute()
+    {
+        if (IsReady)
+        {
+            ActionEvent?.Invoke();
+            yield return new WaitForSeconds(Duration);
+            StartCoroutine(StartCDCoroutine());
+        }
+    }
+
+    private IEnumerator StartCDCoroutine()
+    {
+        IsReady = false;
+        yield return new WaitForSeconds(Cooldown);
+        IsReady = true;
+    }
+}
---
+++ Assets/Scripts/Bosses/TripleTMascot/MoveAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveAttack : BossMove
+{
+    
+    private void Start()
+    {
+        ActionEvent += DebugEvent;
+        Duration = 1;
+        Cooldown = 2;
+        IsReady = true;
+    }
+
+    private void OnDisable()
+    {
+        ActionEvent -= DebugEvent;
+    }
+    public void DebugEvent()
+    {
+        Debug.Log("MoveAttack executed.");
+    }
+}
