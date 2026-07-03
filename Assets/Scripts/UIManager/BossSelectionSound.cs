f6aac6920d58f8ed93d8788bd389ba89a73fddec
Nata <ananthaaulia06@gmail.com>
2026-07-03 17:55:08 +0700

    levelselect Redesign

--- Assets/Scripts/UIManager/BossSelectionSound.cs
+++ Assets/Scripts/UIManager/BossSelectionSound.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+
+public class BossSelectionSound : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
